58679ebcc75aeecf712d2985c6803a705bf6846c
John Bywater <john.bywater@appropriatesoftware.net>
2019-02-10 23:24:03 +0000

    Reordered statements in module.

--- taxisystem.py
+++ taxisystem.py
@@ -7,6 +7,135 @@ from eventsourcing.domain.model.aggregate import AggregateRoot
 from eventsourcing.domain.model.decorators import attribute
 
 
+
+class Rider(AggregateRoot):
+    def __init__(self, **kwargs):
+        super(Rider, self).__init__(**kwargs)
+        self.requests = OrderedDict()
+        self.bookings = OrderedDict()
+
+    class Event(AggregateRoot.Event):
+        pass
+
+    class Created(Event, AggregateRoot.Created):
+        pass
+
+    def request_ride(self, pickup, dropoff):
+        request_id = uuid4()
+        self.__trigger_event__(
+            event_class=self.RideRequested,
+            request_id=request_id,
+            pickup=pickup,
+            dropoff=dropoff
+        )
+        return request_id
+
+    class RideRequested(Event):
+        @property
+        def request_id(self):
+            return self.__dict__['request_id']
+
+        @property
+        def pickup(self):
+            return self.__dict__['pickup']
+
+        @property
+        def dropoff(self):
+            return self.__dict__['dropoff']
+
+        def mutate(self, obj):
+            obj.requests[self.request_id] = RiderRequest(
+                pickup=self.pickup,
+                dropoff=self.dropoff
+            )
+
+    def track_booking_created(self, request_id, booking_id):
+        self.__trigger_event__(
+            event_class=self.BookingCreated,
+            request_id=request_id,
+            booking_id=booking_id
+        )
+
+    class BookingCreated(Event):
+        @property
+        def request_id(self):
+            return self.__dict__['request_id']
+
+        @property
+        def booking_id(self):
+            return self.__dict__['booking_id']
+
+        def mutate(self, obj):
+            obj.requests[self.request_id].booking_id = self.booking_id
+            obj.bookings[self.booking_id] = RiderBooking(request_id=self.request_id)
+
+    def track_ride_offer_accepted(self, booking_id, car_id):
+        self.__trigger_event__(
+            event_class=self.RideOfferAccepted,
+            booking_id=booking_id,
+            car_id=car_id
+        )
+
+    class RideOfferAccepted(Event):
+        @property
+        def booking_id(self):
+            return self.__dict__['booking_id']
+
+        @property
+        def car_id(self):
+            return self.__dict__['car_id']
+
+        def mutate(self, obj):
+            obj.bookings[self.booking_id].car_id = self.car_id
+
+    def track_car_arrived_at_pickup(self, booking_id):
+        self.__trigger_event__(
+            event_class=self.CarArrivedAtPickup,
+            booking_id=booking_id
+        )
+
+    class CarArrivedAtPickup(Event):
+        @property
+        def booking_id(self):
+            return self.__dict__['booking_id']
+
+        def mutate(self, obj):
+            rider_booking = obj.bookings[self.booking_id]
+            assert isinstance(rider_booking, RiderBooking)
+            rider_booking.is_car_arrived_at_pickup = True
+
+    def track_car_arrived_at_dropoff(self, booking_id):
+        self.__trigger_event__(
+            event_class=self.CarArrivedAtDropoff,
+            booking_id=booking_id
+        )
+
+    class CarArrivedAtDropoff(Event):
+        @property
+        def booking_id(self):
+            return self.__dict__['booking_id']
+
+        def mutate(self, obj):
+            rider_booking = obj.bookings[self.booking_id]
+            assert isinstance(rider_booking, RiderBooking)
+            rider_booking.is_car_arrived_at_dropoff = True
+
+
+class RiderRequest(object):
+    def __init__(self, pickup, dropoff):
+        self.booking_id = None
+        self.pickup = pickup
+        self.dropoff = dropoff
+
+
+class RiderBooking(object):
+    def __init__(self, request_id):
+        self.request_id = request_id
+        self.car_id = None
+        self.is_car_arrived_at_pickup = False
+        self.is_car_arrived_at_dropoff = False
+
+
 class Booking(AggregateRoot):
     def __init__(self, rider_id, request_id, pickup, dropoff, **kwargs):
         super(Booking, self).__init__(**kwargs)
@@ -223,132 +352,34 @@ class Car(AggregateRoot):
             obj._is_arrived_at_dropoff = True
 
 
-class Rider(AggregateRoot):
-    def __init__(self, **kwargs):
-        super(Rider, self).__init__(**kwargs)
-        self.requests = OrderedDict()
-        self.bookings = OrderedDict()
+class Riders(ProcessApplication):
+    persist_event_type = Rider.Event
 
-    class Event(AggregateRoot.Event):
-        pass
+    @staticmethod
+    def register_rider():
+        return Rider.__create__()
 
-    class Created(Event, AggregateRoot.Created):
-        pass
+    @staticmethod
+    def policy(repository, event):
+        if isinstance(event, Booking.Created):
+            rider = repository[event.rider_id]
+            assert isinstance(rider, Rider)
+            rider.track_booking_created(event.request_id, event.originator_id)
 
-    def request_ride(self, pickup, dropoff):
-        request_id = uuid4()
-        self.__trigger_event__(
-            event_class=self.RideRequested,
-            request_id=request_id,
-            pickup=pickup,
-            dropoff=dropoff
-        )
-        return request_id
+        elif isinstance(event, Booking.RideOfferAccepted):
+            rider = repository[event.rider_id]
+            assert isinstance(rider, Rider)
+            rider.track_ride_offer_accepted(event.originator_id, event.car_id)
 
-    class RideRequested(Event):
-        @property
-        def request_id(self):
-            return self.__dict__['request_id']
+        elif isinstance(event, Booking.CarArrivedAtPickup):
+            rider = repository[event.rider_id]
+            assert isinstance(rider, Rider)
+            rider.track_car_arrived_at_pickup(event.originator_id)
 
-        @property
-        def pickup(self):
-            return self.__dict__['pickup']
-
-        @property
-        def dropoff(self):
-            return self.__dict__['dropoff']
-
-        def mutate(self, obj):
-            obj.requests[self.request_id] = RiderRequest(
-                pickup=self.pickup,
-                dropoff=self.dropoff
-            )
-
-    def track_booking_created(self, request_id, booking_id):
-        self.__trigger_event__(
-            event_class=self.BookingCreated,
-            request_id=request_id,
-            booking_id=booking_id
-        )
-
-    class BookingCreated(Event):
-        @property
-        def request_id(self):
-            return self.__dict__['request_id']
-
-        @property
-        def booking_id(self):
-            return self.__dict__['booking_id']
-
-        def mutate(self, obj):
-            obj.requests[self.request_id].booking_id = self.booking_id
-            obj.bookings[self.booking_id] = RiderBooking(request_id=self.request_id)
-
-    def track_ride_offer_accepted(self, booking_id, car_id):
-        self.__trigger_event__(
-            event_class=self.RideOfferAccepted,
-            booking_id=booking_id,
-            car_id=car_id
-        )
-
-    class RideOfferAccepted(Event):
-        @property
-        def booking_id(self):
-            return self.__dict__['booking_id']
-
-        @property
-        def car_id(self):
-            return self.__dict__['car_id']
-
-        def mutate(self, obj):
-            obj.bookings[self.booking_id].car_id = self.car_id
-
-    def track_car_arrived_at_pickup(self, booking_id):
-        self.__trigger_event__(
-            event_class=self.CarArrivedAtPickup,
-            booking_id=booking_id
-        )
-
-    class CarArrivedAtPickup(Event):
-        @property
-        def booking_id(self):
-            return self.__dict__['booking_id']
-
-        def mutate(self, obj):
-            rider_booking = obj.bookings[self.booking_id]
-            assert isinstance(rider_booking, RiderBooking)
-            rider_booking.is_car_arrived_at_pickup = True
-
-    def track_car_arrived_at_dropoff(self, booking_id):
-        self.__trigger_event__(
-            event_class=self.CarArrivedAtDropoff,
-            booking_id=booking_id
-        )
-
-    class CarArrivedAtDropoff(Event):
-        @property
-        def booking_id(self):
-            return self.__dict__['booking_id']
-
-        def mutate(self, obj):
-            rider_booking = obj.bookings[self.booking_id]
-            assert isinstance(rider_booking, RiderBooking)
-            rider_booking.is_car_arrived_at_dropoff = True
-
-
-class RiderRequest(object):
-    def __init__(self, pickup, dropoff):
-        self.booking_id = None
-        self.pickup = pickup
-        self.dropoff = dropoff
-
-
-class RiderBooking(object):
-    def __init__(self, request_id):
-        self.request_id = request_id
-        self.car_id = None
-        self.is_car_arrived_at_pickup = False
-        self.is_car_arrived_at_dropoff = False
+        elif isinstance(event, Booking.CarArrivedAtDropoff):
+            rider = repository[event.rider_id]
+            assert isinstance(rider, Rider)
+            rider.track_car_arrived_at_dropoff(event.originator_id)
 
 
 class Office(ProcessApplication):
@@ -419,36 +450,6 @@ class Cars(ProcessApplication):
     @staticmethod
     def register_car():
         return Car.__create__()
-
-
-class Riders(ProcessApplication):
-    persist_event_type = Rider.Event
-
-    @staticmethod
-    def policy(repository, event):
-        if isinstance(event, Booking.Created):
-            rider = repository[event.rider_id]
-            assert isinstance(rider, Rider)
-            rider.track_booking_created(event.request_id, event.originator_id)
-
-        elif isinstance(event, Booking.RideOfferAccepted):
-            rider = repository[event.rider_id]
-            assert isinstance(rider, Rider)
-            rider.track_ride_offer_accepted(event.originator_id, event.car_id)
-
-        elif isinstance(event, Booking.CarArrivedAtPickup):
-            rider = repository[event.rider_id]
-            assert isinstance(rider, Rider)
-            rider.track_car_arrived_at_pickup(event.originator_id)
-
-        elif isinstance(event, Booking.CarArrivedAtDropoff):
-            rider = repository[event.rider_id]
-            assert isinstance(rider, Rider)
-            rider.track_car_arrived_at_dropoff(event.originator_id)
-
-    @staticmethod
-    def register_rider():
-        return Rider.__create__()
 
 
 class TaxiSystem(System):
